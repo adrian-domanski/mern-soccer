1362d0ed619880768b06f3154057378cb1c289da
Adrian Domański <adrian.domanski@prographers.com>
2023-04-07 19:31:13 +0200

    feat: add routes for game update and game delete

--- backend/src/controllers/gameController.ts
+++ backend/src/controllers/gameController.ts
@@ -14,7 +14,7 @@ export const getGameById = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-    const singleGame = await Game.findById(id);
+    const singleGame = await Game.findById({ _id: id });
     res.status(200).json(singleGame);
   } catch (error) {
     res.status(404).json({ error });
@@ -27,5 +27,28 @@ export const createGame = async (req: Request, res: Response) => {
     return res.status(201).json(gameToCreate);
   } catch (error) {
     return res.status(500).json({ msg: "Couldn't create the game" });
+  }
+};
+
+export const updateGame = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const gameToUpdate = await Game.findByIdAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
+  try {
+    return res.status(200).json(gameToUpdate);
+  } catch (error) {
+    return res.status(500).json({ msg: "Couldn't update the game" });
+  }
+};
+
+export const deleteGame = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  await Game.findByIdAndDelete({ _id: id });
+  try {
+    return res.status(200).json({ msg: 'Game deleted' });
+  } catch (error) {
+    return res.status(500).json({ msg: "Couldn't delete the game" });
   }
 };
