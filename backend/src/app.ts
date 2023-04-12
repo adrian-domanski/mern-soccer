d452d1d26b0da2e471582677b5363fdc4abdf8df
Adrian Domański <adrian.domanski@prographers.com>
2023-04-12 21:59:34 +0200

    feat: add express server for /dist build from vite

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import path from 'path';
 
 const app: Application = express();
 app.use(express.json());
@@ -13,5 +14,15 @@ import authRoute from './routes/authRoutes';
 // Declare routes paths
 app.use('/api/games', gameRoute);
 app.use('/api/auth', authRoute);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../frontend/dist'));
+  app.get('*', function (req, res) {
+    res.sendFile('index.html', {
+      root: path.join(__dirname, '../../frontend/dist/'),
+    });
+  });
+}
 
 export { app };
