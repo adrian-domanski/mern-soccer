a559531db7d485039d50196fc0d22617e6c1987f
Adrian Domański <adrian.domanski@prographers.com>
2023-04-15 13:16:46 +0200

    refactor: navbar component refactor

--- frontend/src/components/Layout/NavBar.tsx
+++ frontend/src/components/Layout/NavBar.tsx
@@ -1,266 +1,99 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../store/store';
 import { logOutUser } from '../../features/account/accountSlice';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
+import * as Styled from './Navbar.styles';
+import 'twin.macro';
 
-const settings = [
-  { title: 'Home', route: '/' },
-  { title: 'Create Game', route: '/create-game' },
-  { title: 'Logout', route: '/' }, // add logout function here
-];
+interface LinkRoute {
+  route: string;
+  isAuth: boolean;
+  title: string;
+}
+
+enum LinkNames {
+  Home = 'home',
+  CreateGame = 'createGame',
+  Logout = 'logout',
+  Login = 'login',
+  Register = 'register',
+}
+
+export const NavbarLinks: Record<LinkNames, LinkRoute> = {
+  [LinkNames.Home]: { title: 'Home', route: '/', isAuth: false },
+  [LinkNames.CreateGame]: {
+    route: '/create-game',
+    isAuth: true,
+    title: 'Create Game',
+  },
+  [LinkNames.Logout]: { title: 'Logout', route: '/', isAuth: true },
+  [LinkNames.Login]: { title: 'Login', route: '/login', isAuth: false },
+  [LinkNames.Register]: {
+    title: 'Register',
+    route: '/register',
+    isAuth: false,
+  },
+};
 
 const NavBar = () => {
+  const [isMobileActive, setIsMobileActive] = useState(false);
   const { isLoggedIn } = useAppSelector((state) => state.account);
   const dispatch = useAppDispatch();
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  const getNavbarLinks = () =>
+    Object.entries(NavbarLinks).map(([name, link]) => {
+      if (
+        (!isLoggedIn && !link.isAuth) ||
+        name === LinkNames.Home ||
+        (isLoggedIn && link.isAuth)
+      ) {
+        return (
+          <Styled.NavbarListItem>
+            <Link to={link.route} key={link.route}>
+              <Styled.NavbarLink
+                onClick={() => {
+                  name === LinkNames.Logout && dispatch(logOutUser());
+                  setIsMobileActive(false);
+                }}
+              >
+                {link.title}
+              </Styled.NavbarLink>
+            </Link>
+          </Styled.NavbarListItem>
+        );
+      }
+    });
 
   return (
-    <AppBar className="navbar" position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <Styled.NavbarContainer>
+      <Styled.NavbarContent>
+        <Styled.Logo>
           <Link to="/">
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                color: 'white',
-              }}
-            >
-              FutTube
-            </Typography>
+            <Styled.LogoText>FutTube</Styled.LogoText>
           </Link>
+        </Styled.Logo>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              <Link to="/">
-                <MenuItem onClick={handleCloseNavMenu}>
-                  <Typography fontWeight={600} textAlign="center">
-                    Home
-                  </Typography>
-                </MenuItem>
-              </Link>
-              {isLoggedIn ? (
-                <>
-                  <MenuItem onClick={() => dispatch(logOutUser())}>
-                    <Typography fontWeight={600} textAlign="center">
-                      Log Out
-                    </Typography>
-                  </MenuItem>
-                  <Link to="/create-game">
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography fontWeight={600} textAlign="center">
-                        Create Game
-                      </Typography>
-                    </MenuItem>
-                  </Link>
-                </>
-              ) : (
-                <>
-                  <Link to="/login">
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography fontWeight={600} textAlign="center">
-                        Login
-                      </Typography>
-                    </MenuItem>
-                  </Link>
-                  <Link to="/register">
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography fontWeight={600} textAlign="center">
-                        Register
-                      </Typography>
-                    </MenuItem>
-                  </Link>
-                </>
-              )}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            FutTube
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <Link to="/">
-              <Button
-                onClick={handleCloseNavMenu}
-                sx={{
-                  my: 2,
-                  color: 'white',
-                  display: 'block',
-                  fontWeight: 600,
-                }}
-              >
-                Home
-              </Button>
-            </Link>
-            {isLoggedIn ? (
-              <>
-                <Link to="/create-game">
-                  <Button
-                    onClick={handleCloseNavMenu}
-                    sx={{
-                      my: 2,
-                      color: 'white',
-                      display: 'block',
-                      fontWeight: 600,
-                    }}
-                  >
-                    Create Game
-                  </Button>
-                </Link>
-                <Button
-                  onClick={() => dispatch(logOutUser())}
-                  sx={{
-                    my: 2,
-                    color: 'white',
-                    display: 'block',
-                    fontWeight: 600,
-                  }}
-                >
-                  Log Out
-                </Button>
-              </>
-            ) : (
-              <>
-                <Link to="/login">
-                  <Button
-                    onClick={handleCloseNavMenu}
-                    sx={{
-                      my: 2,
-                      color: 'white',
-                      display: 'block',
-                      fontWeight: 600,
-                    }}
-                  >
-                    Login
-                  </Button>
-                </Link>
-                <Link to="/register">
-                  <Button
-                    onClick={handleCloseNavMenu}
-                    sx={{
-                      my: 2,
-                      color: 'white',
-                      display: 'block',
-                      fontWeight: 600,
-                    }}
-                  >
-                    Register
-                  </Button>
-                </Link>
-              </>
-            )}
-          </Box>
+        <Styled.NavbarList isMobileOpen={isMobileActive}>
+          {getNavbarLinks()}
+        </Styled.NavbarList>
 
-          {isLoggedIn && (
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Avatar" src="https://i.pravatar.cc/80" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <Link to={setting.route}>
-                    <MenuItem
-                      key={setting.title}
-                      onClick={() => {
-                        handleCloseUserMenu();
-                        setting.title === 'Logout' && dispatch(logOutUser());
-                      }}
-                    >
-                      <Typography textAlign="center">
-                        {setting.title}
-                      </Typography>
-                    </MenuItem>
-                  </Link>
-                ))}
-              </Menu>
-            </Box>
-          )}
-        </Toolbar>
-      </Container>
-    </AppBar>
+        <Styled.MobileMenuButton
+          onClick={() => setIsMobileActive(!isMobileActive)}
+        >
+          <Styled.MenuIcon>
+            <FaBars />
+          </Styled.MenuIcon>
+        </Styled.MobileMenuButton>
+
+        {isLoggedIn && (
+          <Styled.Avatar aria-label="Open Settings">
+            <Styled.AvatarImg src="https://i.pravatar.cc/80" alt="Avatar" />
+          </Styled.Avatar>
+        )}
+      </Styled.NavbarContent>
+    </Styled.NavbarContainer>
   );
 };
+
 export default NavBar;
---
+++ frontend/src/components/Layout/Navbar.styles.ts
@@ -0,0 +1,62 @@
+import tw, { styled } from 'twin.macro';
+
+export const NavbarContainer = tw.nav`
+  p-4 
+  text-white 
+  bg-gray-900
+  relative
+`;
+
+export const NavbarContent = tw.div`
+  flex 
+  max-w-6xl
+  items-center 
+  mx-auto
+`;
+
+export const Logo = tw.div`
+  font-bold
+`;
+
+export const LogoText = tw.h1`
+  text-lg
+  md:text-2xl
+`;
+
+export const NavbarListItem = tw.li`
+   text-center border-b border-gray-950 bg-gray-800 even:bg-gray-900 md:bg-transparent
+`;
+
+export const NavbarLink = tw.a`mx-4 font-medium hover:underline py-3 w-full block -ml-1`;
+
+export const NavbarList = styled.ul<{ isMobileOpen: boolean }>`
+  ${tw`absolute scale-y-0 md:scale-y-100 md:space-x-3 transition-transform origin-top top-full md:w-fit w-[calc(100% + 16px)] px-2 -left-2 md:static ml-auto md:flex`}
+  ${({ isMobileOpen }) => isMobileOpen && tw`scale-y-100`}
+`;
+
+export const MobileMenuButton = tw.button`
+  flex 
+  items-center 
+  justify-center
+  border-none 
+  bg-transparent 
+  ml-auto
+  md:hidden
+`;
+
+export const MenuIcon = tw.svg`
+  w-5 
+  h-5 
+  text-white
+`;
+
+export const Avatar = tw.div`
+  hidden 
+  md:block
+`;
+
+export const AvatarImg = tw.img`
+  w-10 
+  h-10 
+  rounded-full
+`;
